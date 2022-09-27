ec3fbba30946dc8e1f241091451213b40c09aeb6
Aamir Khan <aamirkhan8878@gmail.com>
2022-09-27 10:27:28 +0530

    Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
-
-import 'package:firstappflutter/screens/all_task.dart';
+// @dart=2.9
+import 'package:firstappflutter/screens/add_task.dart';
 import 'package:firstappflutter/screens/home_screen.dart';
+import 'package:firstappflutter/screens/task_api.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() => runApp(const MyApp());
+
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -16,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme:ThemeData(
         primarySwatch:Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
